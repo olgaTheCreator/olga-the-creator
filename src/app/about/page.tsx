57d5b3f79258bb15584e3087b2fd57e467a902fd
ojb <curylo.olga@gmail.com>
2023-06-13 18:53:06 +0100

    changed about my page layout to grid, working on responsive sizes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,36 +2,47 @@ import Link from "next/link";
 
 export const AboutMe = () => {
   return (
-    <div className="w-full bg-yellow_bright font-roboto font-medium h-screen p-10 border-x-4 border-b-4 border-black relative ">
-      <div className="bg-white lg:w-2xl p-12 max-w-2xl place-self-end right-20 mr-32 mt-16 border-4 border-black border-dotted absolute">
-        <div className="text-5xl leading-relaxed">
-          <p>Hello,</p>
-          <p>
-            my name is Olga Curylo and I’m a{" "}
-            <span className="font-bold bg-yellow_bright">
-              full-stack developer
-            </span>
-            .
-          </p>
-        </div>
+    <div className="w-full bg-yellow_bright font-roboto font-medium h-screen p-10 border-x-4 border-b-4 border-black relative grid grid-cols-12 grid-rows-6">
+      <div className="bg-white ml-16 2xl:text-5xl lg:text-4xl lg:leading-normal 2xl:leading-relaxed lg:w-2xl 2xl:p-12 lg:p-6 max-w-2xl col-start-6 col-span-5 2xl:row-span-4 lg:row-span-3 mt-16 border-4 border-black border-dotted">
+        <p>Hello,</p>
+        <p>
+          my name is Olga Curylo and I’m a{" "}
+          <span className="font-bold bg-yellow_bright">
+            full-stack developer
+          </span>
+          .
+        </p>
       </div>
-      <div className="bg-white lg:w-72 p-8 max-w-72 mt-10 text-2xl border-4 border-black border-dotted absolute leading-relaxed">
+      <div className="bg-white lg:w-72 2xl:p-8 lg:p-6 max-w-72 text-2xl border-4 border-black border-dotted col-start-3 row-start-1 leading-relaxed">
         <p>
           Checkout my <span className="font-bold">CV </span>and{" "}
           <Link className="text-red_bright underline" href="/portfolio">
             portfolio
-          </Link>{" "}
+          </Link>
           .
         </p>
       </div>
-      <div className="bg-white lg:w-sm p-8 max-w-sm mt-64 left-20 text-2xl border-4 border-black border-dotted absolute leading-relaxed">
+      <div className="bg-white lg:w-sm p-8 max-w-sm col-start-2 col-span-4 row-start-2 row-span-3 mt-20 text-2xl border-4 border-black border-dotted leading-relaxed">
         <p>
-          <ul>
-            I love:
+          <ul className="list-disc">
+            Harvard Cs50 Certificate:
             <li>beating Sekiro on NG+</li>
             <li>techno</li>
             <li>my kids</li>
             <li>routes</li>
+          </ul>
+        </p>
+      </div>
+      <div className="bg-white lg:w-sm p-4 max-w-sm col-start-11 ml-16 col-span-2 mb-16 row-start-2 row-span-3 mt-20 text-lg border-4 border-black border-dotted">
+        <p>
+          <ul className="list-disc list-inside">
+            I love:
+            <li>Sekiro</li>
+            <li>techno</li>
+            <li>my kids</li>
+            <li>routes</li>
+            <li>programming languages</li>
+            <li>Advent of Code</li>
           </ul>
         </p>
       </div>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import { navRoutes } from "../navRoutes";
+import { navRoutes } from "./navRoutes";
 
 export const Navbar = () => {
   const pathname = usePathname();
