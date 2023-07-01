e55e775336996c6e36b35bf7d657c3d4e77f9a60
ojb <curylo.olga@gmail.com>
2023-07-01 16:58:41 +0100

    little improvements to navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -7,12 +7,12 @@ import { navRoutes } from "./navRoutes";
 export const Navbar = () => {
   const pathname = usePathname();
   const activeLink =
-    "h-14 md:h-16 lg:h-24 col-span-2 md:col-span-1 text-4xl p-2 w-full flex place-items-center justify-center lg:p-6 rounded-t-xl border-x-4 border-t-4 border-black lg:text-6xl";
+    "h-20  lg:h-24 col-span-3 md:col-span-1 text-5xl p-4 w-full flex place-items-center justify-center lg:p-6 rounded-t-xl border-x-4 border-t-4 border-black lg:text-6xl";
   const navStyle =
-    "h-12 md:h-14 lg:h-20 w-full p-2 flex place-items-center justify-center lg:p-6  border-t-2 border-b-4 border-x border-black relative ";
+    "h-16 lg:h-20 w-full p-2 flex place-items-center justify-center lg:p-6  border-t-2 border-b-4 border-x border-black relative ";
   return (
     <div className="w-full flex font-vibur">
-      <div className="md:basis-4/5 lg:basis-2/3 basis-full grid md:grid-cols-4 grid-cols-5 place-items-end lg:text-5xl relative md:text-3xl text-2xl">
+      <div className="md:basis-4/5 lg:basis-3/4 xl:basis-2/3 basis-full grid md:grid-cols-4 grid-cols-6 place-items-end lg:text-5xl relative text-4xl">
         {navRoutes.map((link) => {
           const isActive = pathname.startsWith(link.path);
 
@@ -28,7 +28,9 @@ export const Navbar = () => {
             >
               <span
                 className={
-                  !isActive ? "invisible md:visible w-0 md:w-full" : ""
+                  !isActive
+                    ? "invisible md:visible w-0 md:w-full text-center"
+                    : ""
                 }
               >
                 {link.title}
@@ -37,7 +39,7 @@ export const Navbar = () => {
                 className={
                   !isActive
                     ? "visible md:invisible w-full md:w-0 text-center"
-                    : "invisible"
+                    : "invisible w-0"
                 }
               >
                 {link.shortTitle}
@@ -46,7 +48,7 @@ export const Navbar = () => {
           );
         })}
       </div>
-      <div className=" invisible basis-0 md:basis-1/5  md:visible lg:h-24 w-full lg:basis-1/3 border-b-4 border-black"></div>
+      <div className=" invisible basis-0 md:basis-1/5  md:visible lg:h-24 w-full lg:basis-1/4 xl:basis-1/3 border-b-4 border-black"></div>
     </div>
   );
 };
