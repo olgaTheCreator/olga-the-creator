3b338bdac6bf9fce1f48d3a0562926d1d96cd4db
ojb <curylo.olga@gmail.com>
2023-07-01 16:48:52 +0100

    navbar made responsive

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -7,12 +7,12 @@ import { navRoutes } from "./navRoutes";
 export const Navbar = () => {
   const pathname = usePathname();
   const activeLink =
-    "h-24 w-full flex place-items-center justify-center p-6 rounded-t-xl border-x-4 border-t-4 border-black text-6xl";
+    "h-14 md:h-16 lg:h-24 col-span-2 md:col-span-1 text-4xl p-2 w-full flex place-items-center justify-center lg:p-6 rounded-t-xl border-x-4 border-t-4 border-black lg:text-6xl";
   const navStyle =
-    "h-20 w-full flex place-items-center justify-center p-6  border-t-2 border-b-4 border-x border-black relative";
+    "h-12 md:h-14 lg:h-20 w-full p-2 flex place-items-center justify-center lg:p-6  border-t-2 border-b-4 border-x border-black relative ";
   return (
     <div className="w-full flex font-vibur">
-      <div className="basis-2/3 lg:grid lg:grid-cols-4 place-items-end text-5xl relative">
+      <div className="md:basis-4/5 lg:basis-2/3 basis-full grid md:grid-cols-4 grid-cols-5 place-items-end lg:text-5xl relative md:text-3xl text-2xl">
         {navRoutes.map((link) => {
           const isActive = pathname.startsWith(link.path);
 
@@ -26,12 +26,27 @@ export const Navbar = () => {
               href={link.path}
               key={link.title}
             >
-              {link.title}
+              <span
+                className={
+                  !isActive ? "invisible md:visible w-0 md:w-full" : ""
+                }
+              >
+                {link.title}
+              </span>
+              <span
+                className={
+                  !isActive
+                    ? "visible md:invisible w-full md:w-0 text-center"
+                    : "invisible"
+                }
+              >
+                {link.shortTitle}
+              </span>
             </Link>
           );
         })}
       </div>
-      <div className="h-24 w-full basis-1/3 border-b-4 border-black"></div>
+      <div className=" invisible basis-0 md:basis-1/5  md:visible lg:h-24 w-full lg:basis-1/3 border-b-4 border-black"></div>
     </div>
   );
 };
--- src/components/navbar/navRoutes.tsx
+++ src/components/navbar/navRoutes.tsx
@@ -1,26 +1,30 @@
 export const navRoutes = [
   {
     title: "about",
+    shortTitle: "a",
     path: "/about",
     activeStyle: " bg-yellow_bright",
     regularStyle: " bg-yellow_light border-l-2",
   },
   {
     title: "portfolio",
+    shortTitle: "p",
     path: "/portfolio",
     activeStyle: " bg-red_bright",
     regularStyle: " bg-red_light",
   },
   {
     title: "learning",
+    shortTitle: "l",
     path: "/learning",
     activeStyle: " bg-blue_bright",
     regularStyle: " bg-blue_light",
   },
   {
     title: "contact",
+    shortTitle: "c",
     path: "/contact",
     activeStyle: " bg-green_bright",
-    regularStyle: " bg-green_light ",
+    regularStyle: " bg-green_light border-p-2",
   },
 ];
