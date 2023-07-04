546a79df6e9b7a4971003670b0ef18cba5d55434
ojb <curylo.olga@gmail.com>
2023-07-04 14:55:06 +0100

    learning made responsible

--- src/app/learning/page.tsx
+++ src/app/learning/page.tsx
@@ -22,26 +22,28 @@ const learningAreas: LearningArea[] = [
 const LearningResources = () => {
   return (
     <>
-      <div className="w-full relative bg-blue_bright font-roboto font-medium h-max min-h-screen border-x-4 border-b-4 border-black flex flex-col py-16 px-8 items-center gap-10">
-        <h1 className="w-full max-w-screen-xl  text-5xl text-justify">
+      <div className="w-full relative bg-blue_bright font-roboto font-medium h-max min-h-screen border-x-4 border-b-4 border-black flex flex-col p-4 lg:py-8 xl:py-16 lg:px-8 items-center gap-6 xl:gap-10">
+        <h1 className="w-full first-letter:text-4xl md:first-letter:text-5xl max-w-screen-xl lg:text-4xl text-2xl md:text-3xl xl:text-5xl xl:text-justify">
           For people who want to start or expand their self-taught coding
           journey I recommend visiting pages listed below.
         </h1>
-        <h2 className="w-full max-w-screen-xl  text-4xl">
+        <h2 className="md:first-letter:text-4xl first-letter:text-3xl w-full max-w-screen-xl text-xl lg:text-3xl md:text-2xl xl:text-4xl">
           They all were a source of great{" "}
           <span className="font-bold">knowledge</span> and all of the resources
           are <span className="font-bold">free</span>. * next to a link suggests
           more advanced content.
         </h2>
-        <div className="flex gap-8 max-w-screen-xl flex-wrap justify-center">
+        <div className="flex gap-4 md:gap-6 lg:gap-8 max-w-screen-xl flex-wrap justify-center">
           {learningAreas.map((area) => {
             return (
               <div
                 key={area.title}
-                className="bg-white max-w-md mt-5 p-6 h-max  border-4 border-black border-dashed relative"
+                className="bg-white w-full md:max-w-xs lg:max-w-md mt-5 p-4 lg:p-6 h-max  border-4 border-black border-dashed relative"
               >
-                <h1 className="text-3xl font-bold">{area.title}</h1>
-                <ul className="mt-4 text-2xl space-y-2  leading-9 font-semibold list-[square] list-inside">
+                <h1 className=" text-2xl lg:text-3xl font-bold">
+                  {area.title}
+                </h1>
+                <ul className="mt-2 lg:mt-4 text-xl lg:text-2xl xl:space-y-2 leading-8 xl:leading-9 font-semibold list-[square] list-inside">
                   {area.links.map((link) => {
                     return (
                       <li key={link.title}>
