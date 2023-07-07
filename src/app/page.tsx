b3f32960b5692c630aa3ab72a2fa1528e0364137
ojb <curylo.olga@gmail.com>
2023-07-07 17:15:29 +0100

    delete unneded import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import { AnimatePresence } from "framer-motion";
-
 export default function Home({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-8 lg:p-16 xl:p-24">
